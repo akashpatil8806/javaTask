074c48f24c76ab17a63f0fddd5c458724bcb1bbe
Akash Patil <akashgpatil8806@example.com>
2024-05-26 23:31:34 +0530

    all file are added

---
+++ DataType.java
@@ -0,0 +1,18 @@
+// the data type in java
+// int ,har ,array,boolean
+public class DataType {
+    public static void main(String[] args) {
+        int a=10;
+        char b='a';
+        boolean c=true;
+        float f= 2.2f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+
+    
+
+    }
+    
+}
